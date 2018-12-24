853a99c42ec63d806752bca4676fd3b2ed835ca1
Allen Welch <allenjwelch@gmail.com>
2018-12-24 11:54:49 -0500

    updating server

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const fs = require('fs')
+const express = require('express')
 
-const app = express();
+const { createBundleRenderer } = require('vue-server-renderer')
+
+const bundleRenderer = createBundleRenderer(
+    require('./dist/vue-ssr-bundle.json'), {
+        template: fs.readFileSync('./index.html', 'utf-8')
+    }
+)
+
+const app = express()
 const PORT = process.env.PORT || 8000;
 
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
+
+app.use('/dist', express.static('dist'))
+
+app.get('*', (req, res) => {
+    bundleRenderer
+        .renderToStream({ url: req.path })
+        .pipe(res)
+})
 
 app.listen(PORT, function() {
     console.log('App listening on PORT ' + PORT);
